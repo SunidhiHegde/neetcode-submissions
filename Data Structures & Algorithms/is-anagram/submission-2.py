49813e00e04a9ddcbb294b114cc344299bb39b44
Sunidhi Hegde <62884191+SunidhiHegde@users.noreply.github.com>
2026-05-22 02:46:09 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        dict1 = dict()
+        dict2 = dict()
+        for char1,char2 in zip(s,t):
+                dict1[char1] = dict1.get(char1,0) + 1
+                dict2[char2] = dict2.get(char2,0) + 1
+
+        return dict1 == dict2
